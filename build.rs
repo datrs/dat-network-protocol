80ebb6ef0c69e4e61b8820ea3aae7b693933cc00
Yoshua Wuyts <yoshuawuyts@gmail.com>
2018-02-20 01:23:10 +0100

    fix build

--- build.rs
+++ build.rs
@@ -1,21 +1,22 @@
 extern crate protoc_rust;
 
+// Uncomment method below for like local builds.
 fn main() {
-  protoc_rust::run(protoc_rust::Args {
-    out_dir: "src",
-    input: &[
-      "protos/cancel.proto",
-      "protos/data.proto",
-      "protos/data.proto",
-      "protos/feed.proto",
-      "protos/handshake.proto",
-      "protos/have.proto",
-      "protos/info.proto",
-      "protos/request.proto",
-      "protos/unhave.proto",
-      "protos/unwant.proto",
-      "protos/want.proto",
-    ],
-    includes: &["protos"],
-  }).expect("protoc");
+  // protoc_rust::run(protoc_rust::Args {
+  //   out_dir: "src",
+  //   input: &[
+  //     "protos/cancel.proto",
+  //     "protos/data.proto",
+  //     "protos/data.proto",
+  //     "protos/feed.proto",
+  //     "protos/handshake.proto",
+  //     "protos/have.proto",
+  //     "protos/info.proto",
+  //     "protos/request.proto",
+  //     "protos/unhave.proto",
+  //     "protos/unwant.proto",
+  //     "protos/want.proto",
+  //   ],
+  //   includes: &["protos"],
+  // }).expect("protoc");
 }
